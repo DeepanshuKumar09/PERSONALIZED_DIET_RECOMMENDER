800b38dcc5c0b850951d1ed102322400ca607271
Deepanshu Kumar <kumardeepanshu665@gmail.com>
2026-06-17 01:15:58 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-CSS_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/style.css"
-IMAGE_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/images" / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/images/pat.png"
+CSS_PATH = BASE_DIR / "Personalized_Diet_Recommendation/style.css"
+IMAGE_PATH = BASE_DIR / "Personalized_Diet_Recommendation/images" / "Personalized_Diet_Recommendation/images/pat.png"
 
-MODEL_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets" / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets/wellness_model (2).h5"
-SCALER_X_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets" / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets/scaler_X.pkl"
-SCALER_Y_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets" / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets/scaler_y.pkl"
-FEATURES_PATH = BASE_DIR / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets" / "C:/Projects/Langchain_Model/Personalized_Diet_Recommendation/assets/feature_cols.json"
+MODEL_PATH = BASE_DIR / "Personalized_Diet_Recommendation/assets" / "Personalized_Diet_Recommendation/assets/wellness_model (2).h5"
+SCALER_X_PATH = BASE_DIR / "Personalized_Diet_Recommendation/assets" / "Personalized_Diet_Recommendation/assets/scaler_X.pkl"
+SCALER_Y_PATH = BASE_DIR / "Personalized_Diet_Recommendation/assets" / "Personalized_Diet_Recommendation/assets/scaler_y.pkl"
+FEATURES_PATH = BASE_DIR / "Personalized_Diet_Recommendation/assets" / "Personalized_Diet_Recommendation/assets/feature_cols.json"
 
 GEMINI_API_KEY = "ENTER_YOUR_API_KEY"
